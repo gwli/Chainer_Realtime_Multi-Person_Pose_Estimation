c2c1ef453962c7d31c4b5f6a0f0d92f40c2fb787
victor <vili@nvidia.com>
2018-10-26 13:25:13 +0800

    fix typo

--- victor/docker_cmd.sh
+++ victor/docker_cmd.sh
@@ -1,3 +1,3 @@
 docker pull victorgwli/chainer:pip-cuda-8.0
 
-nvidia-docker run --privileged  -e DISPLAY  --net=host --ipc=host -it -v $HOME/.Xautority:/home/nvidia/.Xautoority  -v `pwd`/..:/home/nvidia/samples/Chainer_realtime_Multi-Persion_Pose_estimation  -v /raid/dldata/chainer_coco2017:/home/nvidia/samples/Chainer_realtime_Multi-Persion_Pose_estimation/coco  victorgwli/chainer:pip-cuda-8.0 /bin/bash
+nvidia-docker run --privileged  -e DISPLAY  --net=host --ipc=host -it -v $HOME/.Xauthority:/home/nvidia/.Xauthority  -v `pwd`/..:/home/nvidia/samples/Chainer_realtime_Multi-Persion_Pose_estimation  -v /raid/dldata/chainer_coco2017:/home/nvidia/samples/Chainer_realtime_Multi-Persion_Pose_estimation/coco  victorgwli/chainer:pip-cuda-8.0 /bin/bash
